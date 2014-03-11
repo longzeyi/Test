f75fd6222bdd3fbc4276808d5c1694ba4f900d88
seanlung <seanlung8@gmail.com>
2014-03-11 10:20:16 +0800

    Signed-off-by: seanlung <seanlung8@gmail.com>

--- src/sz/future/md/console/TestMd.java
+++ src/sz/future/md/console/TestMd.java
@@ -2,20 +2,17 @@ package sz.future.md.console;
 
 import org.hraink.futures.jctp.md.JCTPMdApi;
 
-import sz.future.md.spi.Strategy1;
-import sz.future.trader.console.TestTrader;
-
 /**
  * @author Sean
  * 模拟环境交易时间:正常交易时间均可交易,每个交易日晚17：30到凌晨5：00也可进行交易，节假日正常情况下都可进行交易
  */
 public class TestMd {
 	/** 前置机地址 **/
-	static String frontAddr = "tcp://asp-sim2-md1.financial-trading-platform.com:26213";
+//	static String frontAddr = "tcp://asp-sim2-md1.financial-trading-platform.com:26213";
 //	static String frontAddr = "tcp://front112.ctp.gtjafutures.com:41213";
-//	static String frontAddr = "tcp://front111.ctp.gtjafutures.com:41213";
+	static String frontAddr = "tcp://front111.ctp.gtjafutures.com:41213";
 //	static String frontAddr = "tcp://mn101.ctp.gtja-futures.com:41213";
-//	static String frontAddr = "tcp://211.136.142.218:9116";
+//	static String frontAddr = "tcp://gtja-md3.financial-trading-platform.com:41213";
 	/** 行情API **/
 	static JCTPMdApi mdApi;
 	
